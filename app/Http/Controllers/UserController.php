1ad0f928a376f699116d03e4a2d195b3417e8256
fabio-wav <fabio.vale@estudante.ufjf.br>
2023-09-15 13:48:48 -0300

    user controller feita

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+        /**
+         * Display a listing of the resource.
+         */
+        public function index()
+        {
+            $users = User::all();
+            return view('admin.users.index', compact('users'));
+        }
+    
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            $user = new User();
+            return view('admin.users.create', compact('user'));
+        }
+    
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(Request $request)
+        {
+            $data = $request->all();
+    
+            if ($request->hasFile('foto')) {
+                $imagePath = $request->file('foto')->store('images/users');
+                $data['foto'] = $imagePath;
+            }
+            else {
+                $data['foto'] = null;
+            }
+    
+            User::create($data);
+    
+            return redirect()->route('users.index')->with('success', true);
+        }
+    
+        /**
+         * Display the specified resource.
+         */
+        public function show(User $user)
+        {
+            return view ('admin.users.show', compact('user'));
+        }
+    
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(User $user)
+        {
+            return view('admin.users.edit', compact('user'));
+        }
+    
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(Request $request, User $user)
+        {
+            $data = $request->all();
+            $user->update($data);
+    
+            return redirect()->route('users.index')->with('sucess', true);
+        }
+    
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(User $user)
+        {
+            $user->delete();
+            return redirect()->route('users.index')->with('sucess', true);
+        }
+    }
+    
