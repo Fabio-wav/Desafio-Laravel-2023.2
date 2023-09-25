ae13e23e0918e2bd894c8d718a13b5ec3ce6a0c9
fabio-wav <fabio.vale@estudante.ufjf.br>
2023-09-25 16:23:55 -0300

    correcoes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,37 +9,39 @@
 @section('content')
 <div class="row card-header card-outline cor-backend" style="justify-content:center">
 
-        <div class="col-lg-3 col-6">
-            <a href="{{ route('home') }}">
-            <a href="{{ route('gerar.pdf') }}">
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3>Relatório</h3>
-                        <p>Gerar Relatório de Consultas</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                        <i class="far fa-id-badge"></i>
-                    </div>
+    <div class="col-lg-3 col-6">
+        <a href="{{ route('home') }}">
+        <a href="{{ route('gerar.pdf') }}">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>Relatório</h3>
+                    <p>Gerar Relatório de Consultas</p>
                 </div>
-            </a>
-        </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                    <i class="far fa-id-badge"></i>
+                </div>
+            </div>
+        </a>
     </div>
-    @can('ehAdmin', '\App\Models\User')
+</div>
+@can('ehAdmin', '\App\Models\User')
 
-    <div class="row card-header card-outline cor-backend" style="justify-content: center">
-        <div class="col-lg-3 col-6">
-            <h2>Escrever E-mails para Proprietários</h2>
-            <form method="post" action="{{ route('escrever.emails') }}">
-                @csrf
-                <button type="submit" class="btn btn-danger"> 
-                    <i class="fas fa-envelope"></i> Escrever E-mails 
-                </button>
-            </form>
-        </div>
+<div class="row card-header card-outline cor-backend" style="justify-content: center">
+    <div class="col-lg-3 col-6">
+        <h2>Escrever E-mails para Proprietários</h2>
+        <form method="post" action="{{ route('escrever.emails') }}">
+            @csrf
+            <button type="submit" class="btn btn-danger"> 
+                <i class="fas fa-envelope"></i> Escrever E-mails 
+            </button>
+        </form>
     </div>
-    @endcan
+</div>
+@endcan
 @stop
+
+
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
